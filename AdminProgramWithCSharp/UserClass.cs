798923651e813ef112083e7e11c741324d358b91
Lm-Pjunior <julian.vlaming@student.rocvf.nl>
2026-02-12 09:58:42 +0100

    Created the userclass + constructor
    after this i'd propably should be makking the user list and add the 'add' function to the list aswell as the delete option but that'll require me to study it a tiny bit D:

---
+++ AdminProgramWithCSharp/UserClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminProgramWithCSharp
+{
+    internal class UserClass
+    {
+        private string _name;
+        private string _email;
+        private string _password;
+        private int _age;
+
+
+        public UserClass(string name, string email, string password, int age)
+        {
+            _name = name;
+            _email = email;
+            _password = password;
+            _age = age;
+        }
+
+    }
+}
